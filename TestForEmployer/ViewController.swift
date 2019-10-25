8adf1103f6dc90b3de54f70c5c643d6767b49057
lampocka <ilias.ihsanov@yandex.ru>
2019-10-25 23:57:17 +0300

    Initial Commit

---
+++ TestForEmployer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestForEmployer
+//
+//  Created by Ильяс Ихсанов on 25.10.2019.
+//  Copyright © 2019 secret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
